0c7bc170d4384823e0c29eae87a0dc5dc3bcdd6e
minhthong25 <minh35940@gmail>
2025-09-27 00:08:55 +0700

    fe + be(test function)

--- app/src/main/java/com/example/umamuse/MainActivity.java
+++ app/src/main/java/com/example/umamuse/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.umamuse;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        findViewById(R.id.btnTestRacing).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DepositMoneyActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
